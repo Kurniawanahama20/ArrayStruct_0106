8f190efc2dcc1b676f5cd50760bdd77f4c486eb5
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-07 18:26:29 +0700

    commit12

---
+++ struct.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+struct Orang
+{
+    string nama;
+    string alamat;
+    int umur;
+
+};
+
+int main()
+{
+    //deklarasi variabel struct
+    Orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+    
+
+
+}
