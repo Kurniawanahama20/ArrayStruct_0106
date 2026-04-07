cde093b36a184877f0b8b5fc964f2669cb4c1ec4
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-07 17:17:38 +0700

    commit2

--- array.cpp
+++ array.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int main()
+{
+    //deklarasi array disertai instalisasi
+    int dataku[5] = {10, 20 , 30 , 40 ,50 };
+    //deklarasi tanpa instalisasi
+    int datamu[5];
+}
