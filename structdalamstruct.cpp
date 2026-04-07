e1b8f25e49da31a9c37435a82eb05144673d7cb5
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-07 18:41:37 +0700

    commit16

--- structdalamstruct.cpp
+++ structdalamstruct.cpp
@@ -18,5 +18,22 @@ struct Orang
 
 int main ()
 {
-    //deklarasi variabel struct   
+       //deklarasi variabel struct
+    Orang mhs;
+    //mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Isikan data kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "isikan data umur : ";
+    cin >> mhs.umur;
+    cout << endl;
+
+    cout << "Data tampil" << endl;
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "alamat : " << mhs.alamat.desa << endl;
+    cout << "alamat : " << mhs.alamat.kota << endl;
+    cout << "umur : " << mhs.umur << endl;
 }
