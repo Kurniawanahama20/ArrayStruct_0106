815ac3def292b72e705d7d698170b27dc1de1144
Kurniawanahama20 <umykurniawanahama@gmail.com>
2026-04-07 18:36:45 +0700

    commit15

---
+++ structdalamstruct.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+
+};
+
+int main ()
+{
+    //deklarasi variabel struct   
+}
